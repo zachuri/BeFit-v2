8d17a56d516a23d82982c50d14a430a41f4ab25d
zachuri <punsalang.zachary@gmail.com>
2023-06-29 21:08:46 -0700

    dashboard: general setup with links

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link"
 
 import {
   Card,
@@ -7,52 +8,78 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Overview } from "@/components/overview"
+import { RecentActivity } from "@/components/recent-activity"
 
 export default function Page() {
   return (
     <>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-          </CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        {/* Title */}
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+
+        {/* Navigation */}
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Card Title</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">Content Example</div>
-            </CardContent>
+            <Link href={"/weight"}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Weight</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">180 lbs</div>
+                <p className="text-xs text-muted-foreground">
+                  -20.1% from last month
+                </p>
+              </CardContent>
+            </Link>
           </Card>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Card Title</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">Content Example</div>
-            </CardContent>
+            <Link href={"/Diet"}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Diet</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">2300 cals</div>
+                <p className="text-xs text-muted-foreground">
+                  -5.1% from yesterday
+                </p>
+              </CardContent>
+            </Link>
           </Card>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Card Title</CardTitle>
+            <Link href={"/Workout"}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Workouts</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">Leg Day</div>
+                <p className="text-xs text-muted-foreground">Recent workout</p>
+              </CardContent>
+            </Link>
+          </Card>
+        </div>
+
+        {/* Overview/ Recent Activities */}
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+          <Card className="col-span-4">
+            <CardHeader>
+              <CardTitle>Overview</CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">Content Example</div>
+              <Overview />
             </CardContent>
           </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Card Title</CardTitle>
+          <Card className="col-span-3">
+            <CardHeader>
+              <CardTitle>Recent Sales</CardTitle>
+              <CardDescription>You made 265 sales this month.</CardDescription>
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">Content Example</div>
+              <RecentActivity />
             </CardContent>
           </Card>
-        </CardContent>
-      </Card>
+        </div>
+      </div>
     </>
   )
 }
