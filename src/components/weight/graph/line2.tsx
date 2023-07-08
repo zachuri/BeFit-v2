5fcf781948a20faf0b337be6103be8f7566877d5
zachuri <punsalang.zachary@gmail.com>
2023-07-08 03:09:13 -0700

    weight-graph: fixed left side of xaxis to align with right

--- src/components/weight/graph/line2.tsx
+++ src/components/weight/graph/line2.tsx
@@ -28,26 +28,26 @@ const WeightLineGraph2: React.FC<Props> = ({ weights }) => {
     .reverse()
 
   const lastData = formattedData[formattedData.length - 1] // Get the last data point
-  const yAxisStart =
-    lastData && lastData.weight ? Math.floor(lastData.weight / 10) * 10 : 0 // Round down to nearest multiple of 10
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={formattedData}>
+    <ResponsiveContainer width="100%" height={350}>
+      <LineChart margin={{ left: -50, right: 15 }} data={formattedData}>
         <CartesianGrid stroke="none" />
         <XAxis
           dataKey="created_at"
           axisLine={false}
-          tick={{ fontSize: 12, fontWeight: "bold", textAnchor: "end" }}
+          tick={false} // Remove X-axis labels
           interval={Math.ceil(formattedData.length / 5)} // Show only 5 x-axis labels
           tickFormatter={(value, index) =>
-            index === 0 || index === 1 ? value : ""
+            index === 0 || index === formattedData.length - 1 ? value : ""
           } // Show label for first and last x-axis values
+          domain={["dataMin", "dataMax"]} // Adjust the x-axis domain to include the minimum and maximum values
+          padding={{ left: 0 }} // Set padding of -60 on the left side and add padding to the right side
         />
         <YAxis
-          domain={[yAxisStart, "dataMax"]}
+          domain={["dataLow", "dataMax"]}
           axisLine={false}
-          tick={{ fontSize: 12, fontWeight: "bold" }}
+          tick={false} // Remove X-axis labels
           tickCount={2} // Show only 2 y-axis labels (top and bottom)
           tickFormatter={(value, index) =>
             index === 0 || index === 1 ? value : ""
@@ -55,7 +55,13 @@ const WeightLineGraph2: React.FC<Props> = ({ weights }) => {
         />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+        <Line
+          type="linear"
+          dataKey="weight"
+          stroke="#8884d8"
+          dot={false}
+          legendType="none"
+        />
       </LineChart>
     </ResponsiveContainer>
   )
