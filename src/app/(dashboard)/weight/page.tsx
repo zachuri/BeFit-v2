e05d4f31dbffd33ff4e67105a5b5571a9b43976d
zachuri <punsalang.zachary@gmail.com>
2023-07-08 04:24:28 -0700

    weight-graph: made graph top

--- src/app/(dashboard)/weight/page.tsx
+++ src/app/(dashboard)/weight/page.tsx
@@ -17,8 +17,8 @@ export default async function Page() {
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <h2 className="text-3xl font-bold tracking-tight">Weight</h2>
-      <WeightInputCard user_id={session.user.id} weight={weights[0]} />
       <WeightLineGraph2 weights={weights} />
+      <WeightInputCard user_id={session.user.id} weight={weights[0]} />
       <DataTable columns={columns} data={weights} />
       {/* <WeightLineGraph weights={weights} /> */}
     </div>
