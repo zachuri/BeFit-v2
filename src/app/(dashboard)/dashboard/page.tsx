a453b630484b125a5e3bb7fddb8dfd4cb1b95590
zachuri <punsalang.zachary@gmail.com>
2023-07-08 01:10:59 -0700

    clean up

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -14,6 +14,7 @@ import { RecentActivity } from "@/components/recent-activity"
 
 import { getUserWeight } from "./actions"
 import WeightCard from "./weight-card"
+
 export default async function Page() {
   const session = await getServerSession()
   const weights = await getUserWeight(session.user.id)
