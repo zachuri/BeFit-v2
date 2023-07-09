004f318457f1ef3efc53aafcd4ccca7e003d6d65
zachuri <punsalang.zachary@gmail.com>
2023-07-08 17:32:33 -0700

    dashboard/ loading: change name from profile -> dashboardloaing

--- src/app/(dashboard)/dashboard/loading.tsx
+++ src/app/(dashboard)/dashboard/loading.tsx
@@ -1,8 +1,6 @@
 import { CardSkeleton } from "@/components/card-skeleton"
-import { ControlPanelHeader } from "@/components/header"
-import { ControlPanelShell } from "@/components/shell"
 
-export default function ProfileLoading() {
+export default function DashboardLoading() {
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
