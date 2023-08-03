414fc1854169cc0a732dce78485b7b8b13e0d4b5
ZACHURI <punsalang.zachary@gmail.com>
2023-08-03 16:42:47 -0700

    split-display -> split-group-display

--- src/app/(dashboard)/workouts/page.tsx
+++ src/app/(dashboard)/workouts/page.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import { getServerSession } from "@/lib/session"
-import SplitDisplay from "@/components/split/split-display"
+import SplitGroupDisplay from "@/components/split/split-group-display"
 
 import SplitGroupDialog from "../../../components/split/split-group-dialog"
 import { getUserGroupSplits, getUserSplits } from "../dashboard/actions"
@@ -15,7 +15,7 @@ export default async function Page() {
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <SplitGroupDialog user_id={session.user.id} />
-      <SplitDisplay split_group={split_group} splits={splits} />
+      <SplitGroupDisplay split_group={split_group} splits={splits} />
     </div>
   )
 }
