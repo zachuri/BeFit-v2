8aa021e7d1d3d9c6c0509e19ec50067d79c2c6e9
zachuri <punsalang.zachary@gmail.com>
2023-08-11 00:50:54 -0700

    split-cards: changed grid to show three only on large screens

--- src/components/split/split-cards.tsx
+++ src/components/split/split-cards.tsx
@@ -45,7 +45,7 @@ export default function SplitCards({ splits }: Props) {
   }
 
   return (
-    <div className="mt-5 grid gap-5 sm:grid-cols-2 md:grid-cols-3">
+    <div className="mt-5 grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
       {splits.length > 0 ? (
         // If there are splits for the group, display them
         splits.map((split) => (
