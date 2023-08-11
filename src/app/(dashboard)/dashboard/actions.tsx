6581ae72748e9a28d8e84194f7000a616f4e9eb9
zachuri <punsalang.zachary@gmail.com>
2023-08-11 00:48:27 -0700

    actions: change function into arrow functions

--- src/app/(dashboard)/dashboard/actions.tsx
+++ src/app/(dashboard)/dashboard/actions.tsx
@@ -1,169 +1,157 @@
-import { createSupabaseServerClient } from "@/utils/supabase-server"
+import { createSupabaseServerClient } from "@/utils/supabase-server";
+import { Exercises } from "@/types/exercise";
+import { Split, SplitGroup } from "@/types/split";
+import { Weight } from "@/types/weight";
 
-import { Exercise, Exercises } from "@/types/exercise"
-import { Split, SplitGroup } from "@/types/split"
-import { Weight } from "@/types/weight"
-
-export async function getUserWeight(user_id: string): Promise<Weight[]> {
-  const supabase = createSupabaseServerClient()
+export const getUserWeight = async (user_id: string): Promise<Weight[]> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("weight")
     .select("*")
     .eq("user_id", user_id)
-    .order("created_at", { ascending: false })
+    .order("created_at", { ascending: false });
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  return data as Weight[]
-}
+  return data as Weight[];
+};
 
-export async function getUserGroupSplits(
-  user_id: string
-): Promise<SplitGroup[]> {
-  const supabase = createSupabaseServerClient()
+export const getUserGroupSplits = async (user_id: string): Promise<SplitGroup[]> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("split_group")
     .select("*")
     .eq("user_id", user_id)
-    .order("created_at", { ascending: false })
+    .order("created_at", { ascending: false });
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  return data as SplitGroup[]
-}
+  return data as SplitGroup[];
+};
 
-export async function getUserSplits(user_id: string): Promise<Split[]> {
-  const supabase = createSupabaseServerClient()
+export const getUserSplits = async (user_id: string): Promise<Split[]> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("split")
     .select("*")
     .eq("user_id", user_id)
-    .order("created_at", { ascending: true })
+    .order("created_at", { ascending: true });
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  return data as Split[]
-}
+  return data as Split[];
+};
 
-export async function getUserSplitsById(
-  user_id: string,
-  split_id: string
-): Promise<Split[]> {
-  const supabase = createSupabaseServerClient()
+export const getUserSplitsById = async (user_id: string, split_id: string): Promise<Split[]> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("split")
     .select("*")
     .eq("user_id", user_id)
     .eq("split_group_id", split_id)
-    .order("created_at", { ascending: true })
+    .order("created_at", { ascending: true });
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  return data as Split[]
-}
+  return data as Split[];
+};
 
-export async function getSplitGroupName(id: string) {
-  const supabase = createSupabaseServerClient()
+export const getSplitGroupName = async (id: string) => {
+  const supabase = createSupabaseServerClient();
 
   const { data } = await supabase
     .from("split_group")
     .select("name")
     .eq("id", id)
-    .single()
+    .single();
 
-  return data?.name as string
-}
+  return data?.name as string;
+};
 
-export async function getUserSplitById(
-  user_id: string,
-  split_id: string
-): Promise<Split[]> {
-  const supabase = createSupabaseServerClient()
+export const getUserSplitById = async (user_id: string, split_id: string): Promise<Split[]> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("split")
     .select("*")
     .eq("user_id", user_id)
     .eq("split_group_id", split_id)
-    .order("created_at", { ascending: true })
+    .order("created_at", { ascending: true });
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  return data as Split[]
-}
+  return data as Split[];
+};
 
-export async function getSplitName(id: string) {
-  const supabase = createSupabaseServerClient()
+export const getSplitName = async (id: string) => {
+  const supabase = createSupabaseServerClient();
 
   const { data } = await supabase
     .from("split")
     .select("name")
     .eq("id", id)
-    .single()
+    .single();
 
-  return data?.name as string
-}
+  return data?.name as string;
+};
 
-export async function getExercisesForSplit(
-  user_id: string,
-  id: string
-): Promise<string[] | null> {
-  const supabase = createSupabaseServerClient()
+export const getExercisesForSplit = async (user_id: string, id: string): Promise<string[] | null> => {
+  const supabase = createSupabaseServerClient();
 
   const { data } = await supabase
     .from("exercise")
     .select("exercise_id")
     .eq("user_id", user_id)
-    .contains("split_ids", [id])
+    .contains("split_ids", [id]);
 
-  const exerciseIds = data ? data.map((exercise) => exercise.exercise_id) : []
+  const exerciseIds = data ? data.map((exercise) => exercise.exercise_id) : [];
 
-  return exerciseIds as string[] | null
-}
+  return exerciseIds as string[] | null;
+};
 
-export async function getExercisesInfo(ids: string[]): Promise<Exercises[]> {
-  const supabase = createSupabaseServerClient()
+export const getExercisesInfo = async (ids: string[]): Promise<Exercises[]> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("exercises")
     .select("*")
-    .in("id", ids)
+    .in("id", ids);
 
   if (error) {
-    console.error("Error fetching exercises:", error.message)
-    throw error
+    console.error("Error fetching exercises:", error.message);
+    throw error;
   }
 
-  return data as Exercises[]
-}
+  return data as Exercises[];
+};
 
-export async function getExerciseInfo(id: string): Promise<Exercises> {
-  const supabase = createSupabaseServerClient()
+export const getExerciseInfo = async (id: string): Promise<Exercises> => {
+  const supabase = createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("exercises")
     .select("*")
     .eq("id", id)
-    .single()
+    .single();
 
   if (error) {
-    console.error("Error fetching exercises:", error.message)
-    throw error
+    console.error("Error fetching exercises:", error.message);
+    throw error;
   }
 
-  return data as Exercises
-}
+  return data as Exercises;
+};
