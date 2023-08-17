c0fbbd66306946444a6fed1ead59a709c0d92ec9
zachuri <punsalang.zachary@gmail.com>
2023-08-16 22:05:24 -0700

    exercise-info: changed title to Exercise Info/ added name within the card

--- src/app/(dashboard)/workouts/exercise_info/[...exercise_id]/page.tsx
+++ src/app/(dashboard)/workouts/exercise_info/[...exercise_id]/page.tsx
@@ -19,7 +19,7 @@ export default async function Exercise({
 
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
-      <h2 className="text-3xl font-bold tracking-tight">{exercise.name}</h2>
+      <h2 className="text-3xl font-bold tracking-tight">Exercise Info</h2>
       <ExerciseSingleDisplay exercise={exercise} />
     </div>
   )
