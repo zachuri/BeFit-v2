5dc4ec71be1ca1d765c76bdfa29f69c17ffbf083
ZACHURI <punsalang.zachary@gmail.com>
2023-09-01 22:01:46 -0700

    marketing: start with a header and description

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -8,12 +8,12 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Transform Your Fitness Journey with BeFit:
+          <br className="hidden sm:inline" />
+          Your Ultimate Progress Tracking Companion!
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Be able to track your diet, workout and weight.
         </p>
       </div>
       <div className="flex gap-4">
